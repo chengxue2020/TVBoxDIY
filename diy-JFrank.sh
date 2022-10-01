73adee3019c832b7b9643e5f1d0ac061df564e1d
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2022-10-01 10:24:38 +0800

    Update diy-JFrank.sh

--- diy-JFrank.sh
+++ diy-JFrank.sh
@@ -22,8 +22,12 @@ echo "RELEASE_STORE_PASSWORD=TVBoxOSC" >>$CURRENT_DIR/$DIR/gradle.properties
 echo "RELEASE_KEY_PASSWORD=TVBoxOSC" >>$CURRENT_DIR/$DIR/gradle.properties
 #xwalk修复
 #sed -i 's/download.01.org\/crosswalk\/releases\/crosswalk\/android\/maven2/raw.githubusercontent.com\/chengxue2020\/TVBoxDIY\/main/g' $CURRENT_DIR/$DIR/build.gradle
+#更改默认项
+mv $CURRENT_DIR/DIY/app.java $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/base/App.java
 #名称修改
 sed -i 's/TVBox/TVBox_3.0/g' $CURRENT_DIR/$DIR/app/src/main/res/values/strings.xml
+#改版本号及包名
+mv $CURRENT_DIR/DIY/build.gradle $CURRENT_DIR/$DIR/app/build.gradle
 #背景修改
 mv $CURRENT_DIR/DIY/app_bg.png $CURRENT_DIR/$DIR/app/src/main/res/drawable/app_bg.png
 #logo修改
@@ -34,17 +38,46 @@ mv $CURRENT_DIR/DIY/xxxhdpi-app_icon.png $CURRENT_DIR/$DIR/app/src/main/res/draw
 mv $CURRENT_DIR/DIY/app_banner.png $CURRENT_DIR/$DIR/app/src/main/res/drawable/app_banner.png
 #载入动画修改
 mv $CURRENT_DIR/DIY/icon_loading.png $CURRENT_DIR/$DIR/app/src/main/res/drawable/icon_loading.png
+mv $CURRENT_DIR/DIY/anim_loading.xml $CURRENT_DIR/$DIR/app/src/main/res/drawable/anim_loading.xml
+#添加颜色值
+sed '7i \<color name=\"color_FF18D6FF\"\>\#FF18D6FF\<\/color\>' $CURRENT_DIR/$DIR/app/src/main/res/values/colors.xml
+sed '8i \<color name=\"color_8800FF0A\">\#8800FF0A\<\/color\>' $CURRENT_DIR/$DIR/app/src/main/res/values/colors.xml
 #接口内置
 sed -i 's/API_URL, \"\"/API_URL, \"https:\/\/ghproxy.com\/https:\/\/raw.githubusercontent.com\/chengxue2020\/Cat-ports\/main\/main.json\"/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+#EPG
+sed -i 's/getColor\(R.color.color_02F8E1\)/getColor\(R.color.color_1890FF\)/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/adapter/SeriesAdapter.java
+
+#首页直播图标修改
+mv $CURRENT_DIR/icon_live.xml $CURRENT_DIR/$DIR/app/src/main/res/drawable/icon_live.xml
 #首页年月日
 sed -i 's/yyyy\/MM\/dd HH:mm/yyyy\/MM\/dd EE HH:mm/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+#点播界面筛选菜单背景
+sed -i 's/drawable\/shape_dialog_filter_bg/drawable\/shape_dialog_vod_filter_bg/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xml
+mv $CURRENT_DIR/DIY/shape_dialog_vod_filter_bg.xml $CURRENT_DIR/$DIR/app/src/main/res/drawable/shape_dialog_vod_filter_bg.xml
+#点播界面集数背景
+sed -i 's/color_6C3D3D3D/color_66000000/g' $CURRENT_DIR/$DIR/app/src/main/res/drawable/shape_source_series_focus.xml
+#点播界面中间的网速下移
+sed -i 's/dimen/vs_40/dimen/vs_76/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/player_vod_control_view.xml
+#EPG
+sed -i 's/EPG_URL,\"\"/EPG_URL,\"https:\/\/epg.112114.xyz\/\"/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+#直播界面频道列表中跳动条颜色
+sed -i 's/setColor\(Color.RED\)/setColor\(Color.WHITE\)/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/tv/widget/AudioWaveView.java
+#直播界面右上角图标由软件图标更改为载入图标
+sed -i 's/app_icon/icon_loading/g' $CURRENT_DIR/$DIR/app/src/main/res/layout/activity_live_play.xml
+#点播界面进度条背景色
+sed -i 's/color_6C3D3D3D/color_353744/g' $CURRENT_DIR/$DIR/app/src/main/res/drawable/shape_dialog_filter_bg.xml
+#点播界面进度条预缓存颜色
+sed -i 's/color_CC353744/color_8800FF0A/g' $CURRENT_DIR/$DIR/app/src/main/res/drawable/shape_dialog_filter_bg.xml
+#点播界面进度条已播放部分颜色
+sed -i 's/color_353744/color_FF18D6FF/g' $CURRENT_DIR/$DIR/app/src/main/res/drawable/shape_dialog_filter_bg.xml
+#其他
+sed -i 's/color_99FFFFFF/color_99000000/g' $CURRENT_DIR/$DIR/app/src/main/res/drawable/shape_dialog_filter_bg.xml
+sed -i 's/\@color/color_6C3D3D3D/#CC181E28/g' $CURRENT_DIR/$DIR/app/src/main/res/drawable/shape_user_focus.xml
+sed -i 's/radius=\"10mm\"/radius=\"30px\"/g' $CURRENT_DIR/$DIR/app/src/main/res/drawable/shape_user_focus.xml
 
+#增加DOH节点
+mv $CURRENT_DIR/DIY/OkGoHelper.java $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/util/OkGoHelper.java
 
-
-
-
-#取消选集全屏
-sed -i 's/if (showPreview \&\& !fullWindows) toggleFullPreview/\/\/if (showPreview \&\& !fullWindows) toggleFullPreview/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
 #缩略图清晰度修改
 sed -i 's/mContext, 400/mContext, 500/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
 sed -i 's/mContext, 300/mContext, 400/g' $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
